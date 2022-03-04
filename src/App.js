bbfac2312d1b5fa47ba397c642c11e95e5fd4589
Rishabh Sawhney <rishabhsawhney@hotmail.com>
2022-03-04 16:44:08 +0100

    added navbar

--- src/App.js
+++ src/App.js
@@ -3,7 +3,10 @@ import React from 'react';
 import Ingredients from './components/Ingredients/Ingredients';
 
 const App = props => {
-  return <Ingredients />;
+  return (
+    <><div class="header">
+      <h1>Hamming Distance Calculator</h1>
+    </div><Ingredients /></>);
 };
 
 export default App;
